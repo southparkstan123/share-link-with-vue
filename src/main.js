b2d81de4fb72bf27efaaf3c10ef41b56ae5d6008
Dickson Wong - IBM <dicksonwong.ibm@gmail.com>
2019-01-15 22:18:07 +0800

    delete unused code

--- src/main.js
+++ src/main.js
@@ -5,18 +5,15 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import {auth} from './firebase'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-auth.onAuthStateChanged((firebaseUser) => {
-  new Vue({
-    el: '#app',
-    router,
-    components: { App },
-    store,
-    template: '<App/>'
-  })
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  store,
+  template: '<App/>'
 })
